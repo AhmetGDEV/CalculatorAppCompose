7981297ad1703efdc5e5840813cadf59e02e04f0
ahmetsaidgur <77779160+ahmetsaidgur@users.noreply.github.com>
2023-05-05 18:16:07 -0400

    Initial commit

---
+++ app/src/main/java/com/ahmetgur/calculatorcompose/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.ahmetgur.calculatorcompose
+
+sealed class CalculatorOperation(val symbol: String){
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
---
+++ app/src/main/java/com/ahmetgur/calculatorcompose/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.ahmetgur.calculatorcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ahmetgur.calculatorcompose.ui.theme.CalculatorComposeTheme
+import com.ahmetgur.calculatorcompose.ui.theme.MediumGray
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorComposeTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
